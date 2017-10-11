f789988514bb96f38ee62041471c78b730259acf
Julian Slaughter <julians@maxxpotential.com>
2017-10-11 17:55:43 -0400

    finished

--- reader.py
+++ reader.py
@@ -1,10 +1,10 @@
+import csv
 # Now it's your turn to write everything!
 # For this problem, you need to read through a comma-separated document, and sum up
 # the values in each column. THEN, you need to convert that list of values
 # into a sentence using the key at the bottom! Each index corresponds to a letter.
 
-
-
-
-
 key = tuple('abcdefghijklmnopqrstuvwxyz ')
+
+with open("vals.csv") as fin:
+	# Your code goes here!
