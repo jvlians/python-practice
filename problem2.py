f8f5f9c6938d0b9795479d306cf5fa2947a50f28
Julian Slaughter <julians@maxxpotential.com>
2017-10-11 19:41:06 -0400

    fixed prints

--- problem2.py
+++ problem2.py
@@ -1,15 +1,15 @@
 def concat(str1, str2):
 	# Now, write a program that combines the first and second strings 
 	# with a space between!
-	
+	return str1+" "+str2
 	# Your code goes here!
 	
 
 
 str1 = "Hello"
 str2 = "World"
-print concat(str1, str2)
+print(concat(str1, str2))
 if concat(str1, str2) == "Hello World":
-	print 'success!'
+	print('success!')
 else:
-	print 'try again!'
+	print('try again!')
